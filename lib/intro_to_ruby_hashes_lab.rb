835907d79386a7dfe37289fdb86919d811a4d53e
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-10 17:46:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,8 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key = :name
+  
   my_hash = {
-    name: value
+    key => value
   }
 end
 
